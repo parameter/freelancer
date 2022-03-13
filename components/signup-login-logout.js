7721fd39f64584e19aea80eea1adc604de2c064f
parameter <paer.henriksson@yahoo.com>
2022-03-13 19:42:09 +0100

    fixing it 00

--- components/signup-login-logout.js
+++ components/signup-login-logout.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-export default (props) => {
+export default function SignupLOginLOgout(props) {
 
     const signUp = async (event) => {
         event.preventDefault();
