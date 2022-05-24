2623cdc6d24b178fb15645ec389706d6c2c66e3d
parameter <paer.henriksson@yahoo.com>
2022-05-24 16:52:49 +0200

    getting it styled up 28

--- pages/index.js
+++ pages/index.js
@@ -55,6 +55,8 @@ export default function Home() {
         <p>Now, once we can see through the noise and the background radiation of the universe it&apos;s time to get some work done.</p>
         <p>I am a developer based in Stockholm who likes electronic music &#123;insert_icon&#125; and discgolf &#123;insert_icon&#125;.</p>
         <p>As I like to try new things I hosted this site on the peer-to-peer network Interplanetary file system (ipfs). It&apos;s not hosted on an ordinary server but a more decentralized method where the data is stored and retried from multiple nodes where all users share the burden of holding a bit of data for everyone else.</p>
+        <p>By the way, there are 15 versions of this site, can you find them?</p>
+        <p>Tweek the parameters!</p>
         <div style={{backgroundImage: 'url(images/me.webp)'}} className="article__post-user"></div>
       </div>
       <div className="article__wrap article__post">
