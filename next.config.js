cb59f15974ceb9290a955990a5d388fb327c029e
parameter <paer.henriksson@yahoo.com>
2022-05-26 17:02:54 +0200

    getting it styled up 39

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' }
+      }
+  },
   reactStrictMode: true,
-  exportTrailingSlash: false
+  trailingSlash: false
 }
 
 module.exports = nextConfig;
