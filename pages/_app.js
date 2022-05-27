75ac591e53ee06bea668a75ee6118e2d339eba1f
parameter <paer.henriksson@yahoo.com>
2022-05-27 13:27:32 +0200

    getting it styled up 45

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { SessionProvider } from "next-auth/react";
+import Head from 'next/head';
 import { useRef, useState } from 'react';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -56,6 +57,11 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
   }
 
   return <SessionProvider session={session}>
+           <Head>
+             <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+             <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+             <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+           </Head>
            <div className="base">
             <div className="base__page">
               <div className="base__content">
