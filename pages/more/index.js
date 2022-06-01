8a00175d99ad6caaf2a61ea36112f653d0fa8fc3
parameter <paer.henriksson@yahoo.com>
2022-06-01 13:06:48 +0200

    getting it styled up 70

--- pages/more/index.js
+++ pages/more/index.js
@@ -19,6 +19,7 @@ export default function FreeNft() {
     <>
         <div className="article__wrap article__post">
             <p>Ask me for modern realtime sites made in react and next-js (like this one), wordpress CMS solutions modelled to your business and ideas, static sites (headless) or ask me to be part of your team</p>
+            <p>Maybe you want your site to be a bit more decentralized and host it on ipfs (interplanetary file system), create a utility NFT for whatever you are doing or just for discussing an idea.</p>
             <p>I enjoy making accessible interfaces and user experiences. If it looks good and feels great it can take over the world.</p>
             <p><a href="mailto:parameter64@proton.me">parameter64@proton.me</a></p>
             <p><a href="tel:0046738790924">+46 73 87 909 24</a></p>
