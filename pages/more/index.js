b577a505dd1c13f4f1a256f50787cf24074e6003
parameter <paer.henriksson@yahoo.com>
2022-06-01 12:36:03 +0200

    getting it styled up 69

--- pages/more/index.js
+++ pages/more/index.js
@@ -18,8 +18,10 @@ export default function FreeNft() {
   return (
     <>
         <div className="article__wrap article__post">
-            
-            <p></p>
+            <p>Ask me for modern realtime sites made in react and next-js (like this one), wordpress CMS solutions modelled to your business and ideas, static sites (headless) or ask me to be part of your team</p>
+            <p>I enjoy making accessible interfaces and user experiences. If it looks good and feels great it can take over the world.</p>
+            <p><a href="mailto:parameter64@proton.me">parameter64@proton.me</a></p>
+            <p><a href="tel:0046738790924">+46 73 87 909 24</a></p>
             <div style={{backgroundImage: 'url(/images/me.webp)'}} className="article__post-icon"></div>
         </div>
     </>
